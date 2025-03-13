b41850cf40d7a097022659e28c4910e85036caa7
vernonthedev <techjaja2@gmail.com>
2025-03-13 05:21:25 -0400

    Cleaned the project.

---
+++ app/profile.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native' 
+
+export default function Profile(){
+    return (
+        <>
+        <View>
+            <Text>Welcome to my profile page</Text>
+        </View>
+        </>
+    )
+} 
